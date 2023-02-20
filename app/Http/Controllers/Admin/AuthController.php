01be6c34b892567991fccfa9fa55c25cc65260ff
scalarvector1003 <happysmile911003@gmail.com>
2023-02-21 00:00:32 +0800

    set up swagger api

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -9,6 +9,46 @@ use App\Http\Controllers\Controller;
 use App\Models\Admin\User;
 
 class AuthController extends Controller {
+
+
+    /**
+    * @OA\Post(
+    * path="/api/admin/login",
+    * operationId="adminLogin",
+    * tags={"Admin Login"},
+    * summary="Admin Login",
+    * description="This is Admin Login API",
+    *     @OA\RequestBody(
+    *         @OA\JsonContent(),
+    *         @OA\MediaType(
+    *            mediaType="multipart/form-data",
+    *            @OA\Schema(
+    *               type="object",
+    *               required={"username", "password"},
+    *               @OA\Property(property="username", type="username"),
+    *               @OA\Property(property="password", type="password")
+    *            ),
+    *        ),
+    *    ),
+    *      @OA\Response(
+    *          response=201,
+    *          description="Login Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=200,
+    *          description="Login Successfully",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(
+    *          response=422,
+    *          description="Unprocessable Entity",
+    *          @OA\JsonContent()
+    *       ),
+    *      @OA\Response(response=400, description="Bad request"),
+    *      @OA\Response(response=404, description="Resource Not Found"),
+    * )
+    */
     /* Login action. */
     public function login(Request $request) {
         $username = $request->input('username');
@@ -31,6 +71,54 @@ class AuthController extends Controller {
         return $this->respondWithToken($accessToken, 'Login successfully.', auth()->guard('admin')->user());
     }
 
+    /**
+     * @OA\Post(
+     ** path="/api/admin/register",
+     *   tags={"Admin Register"},
+     *   summary="Admin Register",
+     *   operationId="adminRegister",
+     *   description="This is Admin Register API",
+     *   @OA\Parameter(
+     *      name="username",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Parameter(
+     *      name="password",
+     *      in="query",
+     *      required=true,
+     *      @OA\Schema(
+     *           type="string"
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=201,
+     *       description="Success",
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *      )
+     *   ),
+     *   @OA\Response(
+     *      response=401,
+     *       description="Unauthenticated"
+     *   ),
+     *   @OA\Response(
+     *      response=400,
+     *      description="Bad Request"
+     *   ),
+     *   @OA\Response(
+     *      response=404,
+     *      description="not found"
+     *   ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *)
+     **/
     /* Register action. */
     public function register(Request $request) {
         $username = $request->input('username');
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,32 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Casino API documentation",
+     *      description="L5 Swagger OpenApi description",
+     *      @OA\Contact(
+     *          email="zerry319.wit@gmail.com"
+     *      ),
+     *      @OA\License(
+     *          name="Apache 2.0",
+     *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Local API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Casino",
+     *     description="API Endpoints of Casino"
+     * )
+     */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
